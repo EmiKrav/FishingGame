c7530c29e2e405bd57d2628cf06fd44c053072e5
Emikra <emikra@ktu.lt>
2024-02-19 17:19:50 +0200

    1

--- app/src/main/java/com/example/fishinggame/CathingFragment.kt
+++ app/src/main/java/com/example/fishinggame/CathingFragment.kt
@@ -6,19 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.example.fishinggame.databinding.FragmentCathingBinding
 import com.example.fishinggame.databinding.FragmentGameScreenBinding
 
-class GameScreenFragment : Fragment() {
+
+class CathingFragment : Fragment() {
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentGameScreenBinding.inflate(inflater)
-         binding.imageView
-           .setOnClickListener {
-             findNavController()
-               .navigate(R.id.action_gameScreenFragment_to_cathingFragment)
-         }
+        val binding = FragmentCathingBinding.inflate(inflater)
 
         return binding.root
     }
